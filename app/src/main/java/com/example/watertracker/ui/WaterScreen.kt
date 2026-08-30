4442a86ecbcf40d2e37f18345474549da49fb361
egor-ver <e.verovsky@yandex.ru>
2026-08-31 00:16:46 +0300

    fix: remove WaterScreen invalid import

--- app/src/main/java/com/example/watertracker/ui/WaterScreen.kt
+++ app/src/main/java/com/example/watertracker/ui/WaterScreen.kt
@@ -1,6 +1,6 @@
 package com.example.watertracker.ui
 
-пшimport androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
